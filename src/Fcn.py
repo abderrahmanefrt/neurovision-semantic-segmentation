0ed8df0561bee7611f04de5e0f4c361cc8af6a44
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2026-06-04 22:56:04 +0100

    FCN

---
+++ src/Fcn.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Define the convolutional block
+class conv_block(nn.Module):
+   def __init__(self, in_ch,out_ch):
+      super().__init__()
+      self.block= nn.Sequential(
+         nn.Conv2d(in_ch,out_ch,kernel_size=3,padding=1),
+         nn.ReLU(inplace=True),
+         nn.Conv2d(in_ch,out_ch,kernel_size=3,padding=1),
+         nn.ReLU(inplace=True),
+      )
+
+
+      def forward(self,x):
+         return self.block(x)
